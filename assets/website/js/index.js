2b76dd553e0a6fc5aab86e4fe6ff487be4285faf
Alexander Schranz <alexander@sulu.io>
2020-09-21 13:28:24 +0200

    Add google map example component

--- assets/website/js/index.js
+++ assets/website/js/index.js
@@ -1,9 +1,13 @@
 import 'core-js/features/object/assign'; // polyfill for IE support
 import web from '@sulu/web';
 import $ from 'jquery';
+import Map from './components/map';
 
 window.$ = window.jQuery = $;
 window.web = web;
+
+// register components
+web.registerComponent('map', Map);
 
 // The started components are written into a variable in the `base.html.twig` file
 // this allows us to async load the whole javascript code and start the components
